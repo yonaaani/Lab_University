0eecbb803feb5a5dab1f4b1d29dbc4d276d99e6c
yonaaani <95050287+yonaaani@users.noreply.github.com>
2022-11-01 17:09:16 +0200

    Add files via upload

---
+++ Lab_2k/Lab_1sm/JS_Galochkin/JS_07/primJs.js
@@ -0,0 +1,7 @@
+a = "background-color: # 00ffff; color: # ff00ff;"
+a + = "font-size: 24pt; font-family: 'Times New Roman'"
+naim = 'Мережа магазинів "ВСЕ ДЛЯ БУДИНКУ"'
+var da = new Date ()
+d = da.getDate () + "." + (da.getMonth () + 1) + "." + da.getFullYear ()
+document.write ( '<P align = center style = "' + a + '">' +
+naim + '</P> <P> Сьогодні' + d + '</P>')
