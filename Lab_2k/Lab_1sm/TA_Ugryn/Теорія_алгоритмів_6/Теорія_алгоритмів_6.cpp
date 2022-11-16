6db5b2f8a16cc19c197db39e45b6a612a8c40d87
yonaaani <95050287+yonaaani@users.noreply.github.com>
2022-11-16 19:43:57 +0200

    download TA_6 Ugryn

---
+++ Lab_2k/Lab_1sm/TA_Ugryn/Теорія_алгоритмів_6/Теорія_алгоритмів_6.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Перетворення до суми
+float sum(float x, int k)
+{
+    int n = 0;
+    float a = 1, sum, r;
+    sum = 0;
+    for (k = 0; k <= n; k++)
+    {
+        sum += a;
+        r = (2*x*(k/2 + 35)+k);
+        a = a * r;
+    }
+    return sum;
+}
+double t(double x)
+{
+    return sum(0, 10);
+}
+
+double foo(int n, int t = 100) {
+    if (t > n) return 0;
+    return (2*foo(n,t + 1)*((n/2)+35) + n);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ukr");
+
+    double n, T;
+    cout << " n = "; cin >> n;
+    T = 2 * t(n) * (n / 2 + 35) + n;
+    cout << " Перетворення до суми: " << endl;
+    cout << " T(n) = 2T(n/2 + 35) + n = " << T << endl;
+    cout << foo(n);
+    system("pause");
+    return 0;
+}
