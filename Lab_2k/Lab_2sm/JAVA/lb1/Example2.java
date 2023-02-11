608a3fc2d5ef9c384ef809b4eada327579e356e8
yonaaani <95050287+yonaaani@users.noreply.github.com>
2023-02-11 15:53:49 +0200

    Add files via upload

---
+++ Lab_2k/Lab_2sm/JAVA/lb1/Example2.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+public class Example2
+{
+    public static void main(String[] args)
+    {
+        char inChar;
+        System.out.println("Input symbol");
+        try {
+            inChar=(char)System.in.read();
+            System.out.println("Your symbol is "+inChar);
+        }
+        catch (IOException e)
+        {System.out.println("Input ERROR");
+        }
+    }
+}
---
+++ Lab_2k/Lab_2sm/JAVA/lb1/Hello.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Введіть перше число: ");
+        float num1 = scan.nextFloat();
+
+        System.out.print("Введіть друге число: ");
+        float num2 = scan.nextFloat();
+
+        float res1 = num1 + num2;
+        float res2 = num1 - num2;
+        float res3 = num1 * num2;
+        float res4 = num1 / num2;
+
+        System.out.println("Результат: ");
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4);
+
+    }
+}
