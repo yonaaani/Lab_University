dee9cf1a5cce775a4f9e52c22aee75e94825d10f
yonaaani <95050287+yonaaani@users.noreply.github.com>
2023-09-25 14:42:38 +0300

    upload java.spring.lb19

---
+++ Lab_3k/Lab_1sm/JAVA_Spring/lb19/SpringBootGroovy/src/test/java/org/o7planning/sbgroovy/SpringBootGroovyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbgroovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
