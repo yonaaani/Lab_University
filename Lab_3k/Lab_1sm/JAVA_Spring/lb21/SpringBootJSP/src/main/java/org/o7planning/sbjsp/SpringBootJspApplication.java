284aaaa06c59d3cea18b4c844e4d60dfa0c35cc7
yonaaani <95050287+yonaaani@users.noreply.github.com>
2023-10-12 19:03:17 +0300

    upload java.spring.lb21

---
+++ Lab_3k/Lab_1sm/JAVA_Spring/lb21/SpringBootJSP/src/main/java/org/o7planning/sbjsp/SpringBootJspApplication.java
@@ -0,0 +1,9 @@
+package org.o7planning.sbjsp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootJspApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJspApplication.class, args);
+    }
+}
---
+++ Lab_3k/Lab_1sm/JAVA_Spring/lb21/SpringBootJSP/src/test/java/org/o7planning/sbjsp/SpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
