d4409d2d6e03b7084fd40325039b133891d0f98e
yonaaani <95050287+yonaaani@users.noreply.github.com>
2023-10-12 19:03:51 +0300

    upload java.spring.lb22

---
+++ Lab_3k/Lab_1sm/JAVA_Spring/lb22/SpringBootDefaultLogging/src/main/java/org/o7planning/sblogging/SpringBootDefaultLoggingApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.sblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDefaultLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDefaultLoggingApplication.class, args);
+    }
+
+}
