b8fc800c6c53c847705ab2b2f718292eb6f9154c
yonaaani <95050287+yonaaani@users.noreply.github.com>
2023-10-19 20:09:06 +0300

    upload java.spring.lb23

---
+++ Lab_3k/Lab_1sm/JAVA_Spring/lb23/SpringBootMultiViewResolver/src/main/java/org/o7planning/sbmultiviewresolver/SpringBootMultiViewResolverApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbmultiviewresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiViewResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiViewResolverApplication.class, args);
+    }
+
+}
